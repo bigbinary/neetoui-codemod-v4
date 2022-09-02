2b61f98e34c6e8834c9c1f72a5c87b045bdcfa9b
amaldinesh7 <amalkdinesh@gmail.com>
2022-09-02 19:01:00 +0530

    Readme content removed

--- bin/cli.js
+++ bin/cli.js
@@ -1,24 +1,24 @@
-const globby = require('globby');
+const globby = require("globby");
 // const inquirer = require("inquirer");
-const meow = require('meow');
-const path = require('path');
-const execa = require('execa');
-const chalk = require('chalk');
-const isGitClean = require('is-git-clean');
+const meow = require("meow");
+const path = require("path");
+const execa = require("execa");
+const chalk = require("chalk");
+const isGitClean = require("is-git-clean");
 
-const transformerDirectory = path.join(__dirname, '../', 'transforms');
-const jscodeshiftExecutable = require.resolve('.bin/jscodeshift');
+const transformerDirectory = path.join(__dirname, "../", "transforms");
+const jscodeshiftExecutable = require.resolve(".bin/jscodeshift");
 
 const transformers = [
-  'Button',
-  'Select',
-  'Dropdown',
-  'Avatar',
-  'Modal',
-  'Pane',
-  'Alert',
-  'Tag',
-  'Classnames',
+  "Button",
+  "Select",
+  "Dropdown",
+  "Avatar",
+  "Modal",
+  "Pane",
+  "Alert",
+  "Tag",
+  "Classnames",
 ];
 
 // const TRANSFORMER_INQUIRER_CHOICES = [
@@ -58,12 +58,12 @@ const transformers = [
 
 function checkGitStatus(force) {
   let clean = false;
-  let errorMessage = 'Unable to determine if git directory is clean';
+  let errorMessage = "Unable to determine if git directory is clean";
   try {
     clean = isGitClean.sync(process.cwd());
-    errorMessage = 'Git directory is not clean';
+    errorMessage = "Git directory is not clean";
   } catch (err) {
-    if (err && err.stderr && err.stderr.indexOf('Not a git repository') >= 0) {
+    if (err && err.stderr && err.stderr.indexOf("Not a git repository") >= 0) {
       clean = true;
     }
   }
@@ -72,14 +72,14 @@ function checkGitStatus(force) {
     if (force) {
       console.log(`WARNING: ${errorMessage}. Forcibly continuing.`);
     } else {
-      console.log('Thank you for using react-codemods!');
+      console.log("Thank you for using react-codemods!");
       console.log(
         chalk.yellow(
-          '\nBut before we continue, please stash or commit your git changes.'
+          "\nBut before we continue, please stash or commit your git changes."
         )
       );
       console.log(
-        '\nYou may use the --force flag to override this safety check.'
+        "\nYou may use the --force flag to override this safety check."
       );
       process.exit(1);
     }
@@ -94,24 +94,24 @@ function runTransform({ files, flags, transformer }) {
   const { dry, print, explicitRequire } = flags;
 
   if (dry) {
-    args.push('--dry');
+    args.push("--dry");
   }
   if (print) {
-    args.push('--print');
+    args.push("--print");
   }
 
-  if (explicitRequire === 'false') {
-    args.push('--explicit-require=false');
+  if (explicitRequire === "false") {
+    args.push("--explicit-require=false");
   }
 
-  args.push('--verbose=2');
+  args.push("--verbose=2");
 
-  args.push('--ignore-pattern=**/node_modules/**');
+  args.push("--ignore-pattern=**/node_modules/**");
 
-  args.push('--parser', 'babel');
-  args.push('--extensions=jsx,js');
+  args.push("--parser", "babel");
+  args.push("--extensions=jsx,js");
 
-  args = args.concat(['--transform', transformerPath]);
+  args = args.concat(["--transform", transformerPath]);
 
   if (flags.jscodeshift) {
     args = args.concat(flags.jscodeshift);
@@ -119,10 +119,10 @@ function runTransform({ files, flags, transformer }) {
 
   args = args.concat(files);
 
-  console.log(`Executing command: jscodeshift ${args.join(' ')}`);
+  console.log(`Executing command: jscodeshift ${args.join(" ")}`);
 
   const result = execa.sync(jscodeshiftExecutable, args, {
-    stdio: 'inherit',
+    stdio: "inherit",
     stripEof: false,
   });
 
@@ -133,7 +133,7 @@ function runTransform({ files, flags, transformer }) {
 
 function expandFilePathsIfNeeded(filesBeforeExpansion) {
   const shouldExpandFiles = filesBeforeExpansion.some((file) =>
-    file.includes('*')
+    file.includes("*")
   );
   return shouldExpandFiles
     ? globby.sync(filesBeforeExpansion)
@@ -143,28 +143,28 @@ function expandFilePathsIfNeeded(filesBeforeExpansion) {
 function run() {
   const cli = meow(
     {
-      description: 'Codemods for updating React APIs.',
+      description: "Codemods for updating React APIs.",
       help: `
-    Usage
-      $ npx react-codemod <transform> <path> <...options>
+      Usage
+        $ npx react-codemod <transform> <path> <...options>
 
-        transform    One of the choices from https://github.com/reactjs/react-codemod
-        path         Files or directory to transform. Can be a glob like src/**.test.js
+          transform    One of the choices from https://github.com/reactjs/react-codemod
+          path         Files or directory to transform. Can be a glob like src/**.test.js
 
-    Options
-      --force            Bypass Git safety checks and forcibly run codemods
-      --dry              Dry run (no changes are made to files)
-      --print            Print transformed files to your terminal
-      --explicit-require Transform only if React is imported in the file (default: true)
+      Options
+        --force            Bypass Git safety checks and forcibly run codemods
+        --dry              Dry run (no changes are made to files)
+        --print            Print transformed files to your terminal
+        --explicit-require Transform only if React is imported in the file (default: true)
 
-      --jscodeshift  (Advanced) Pass options directly to jscodeshift
-    `,
+        --jscodeshift  (Advanced) Pass options directly to jscodeshift
+      `,
     },
     {
-      boolean: ['force', 'dry', 'print', 'explicit-require', 'help'],
-      string: ['_'],
+      boolean: ["force", "dry", "print", "explicit-require", "help"],
+      string: ["_"],
       alias: {
-        h: 'help',
+        h: "help",
       },
     }
   );
@@ -177,7 +177,7 @@ function run() {
   const filesExpanded = expandFilePathsIfNeeded([filesBeforeExpansion]);
 
   if (!filesExpanded.length) {
-    console.log(`No files found matching ${filesBeforeExpansion.join(' ')}`);
+    console.log(`No files found matching ${filesBeforeExpansion.join(" ")}`);
     return null;
   }
 
